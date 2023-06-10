d71f6ca99c36aa9b7316413b4378608d97a8933d
Behnam Sajadifar <beh.sajadi94@gmail.com>
2023-06-10 17:32:03 +0330

    feat: add autocomplete location names feature

--- src/app/pages/weather-search-page/weather-search-page.component.ts
+++ src/app/pages/weather-search-page/weather-search-page.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { ApiService, GeoData } from '@inWeather/core';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import {
+  Observable,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  switchMap,
+} from 'rxjs';
 
+/**
+ * Weather search page component class.
+ */
 @Component({
   selector: 'app-weather-search-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    NzAutocompleteModule,
+    NzInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './weather-search-page.component.html',
   styleUrls: ['./weather-search-page.component.less'],
 })
-export class WeatherSearchPageComponent {}
+export class WeatherSearchPageComponent implements OnInit {
+  private readonly _api = inject(ApiService);
+
+  inputCtrl = new FormControl<string>('');
+  options$?: Observable<GeoData[]>;
+
+  /**
+   * Component onInit hook.
+   */
+  ngOnInit(): void {
+    this.options$ = this.inputCtrl.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      filter((inputTxt): inputTxt is string => inputTxt !== null),
+      switchMap((inputTxt) => {
+        return this._api.getCityGeoData(inputTxt, 5);
+      })
+    );
+  }
+}
