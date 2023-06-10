eace67e6e4f332810203e1444979e0c39d6d67ac
Behnam Sajadifar <beh.sajadi94@gmail.com>
2023-06-10 08:19:22 +0330

    chore: install and configure ng-zorro-antd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NzCollapseModule } from 'ng-zorro-antd/collapse';
 
 /**
  * App component class.
@@ -8,10 +9,27 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NzCollapseModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
   title = 'inWeather';
+  panels = [
+    {
+      active: true,
+      name: 'This is panel header 1',
+      disabled: false,
+    },
+    {
+      active: false,
+      disabled: false,
+      name: 'This is panel header 2',
+    },
+    {
+      active: false,
+      disabled: true,
+      name: 'This is panel header 3',
+    },
+  ];
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import en from '@angular/common/locales/en';
+import { NZ_I18N, en_GB } from 'ng-zorro-antd/i18n';
 import { routes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+registerLocaleData(en);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    importProvidersFrom([BrowserModule, BrowserAnimationsModule]),
+    provideRouter(routes),
+    { provide: NZ_I18N, useValue: en_GB },
+  ],
 };
