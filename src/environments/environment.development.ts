32b6cf0ff896c561f63852092063933bbb45d3ee
Behnam Sajadifar <beh.sajadi94@gmail.com>
2023-06-11 16:31:45 +0330

    chore: migrate to https api base url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -6,7 +6,7 @@ import { Environment } from '@inWeather/core';
 
 export const environment: Environment = {
   WEATHER_API: {
-    BASE_URL: 'http://api.openweathermap.org',
+    BASE_URL: 'https://api.openweathermap.org',
     API_KEY: '7b4fa149865448cc62b6357bdd351382',
   },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,7 @@ import { Environment } from '@inWeather/core';
 
 export const environment: Environment = {
   WEATHER_API: {
-    BASE_URL: 'http://api.openweathermap.org',
+    BASE_URL: 'https://api.openweathermap.org',
     API_KEY: '25facc681db44eeb6d544cc1f1e739b8',
   },
 };
