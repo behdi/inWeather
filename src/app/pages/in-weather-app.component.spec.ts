a31513ea3688430cbe5e6675942b47901c5c957b
Behnam Sajadifar <beh.sajadi94@gmail.com>
2023-06-12 09:47:48 +0330

    test: add tests to in weather app component

--- src/app/pages/in-weather-app.component.spec.ts
+++ src/app/pages/in-weather-app.component.spec.ts
@@ -4,6 +4,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { TranslocoTestingModule } from '@ngneat/transloco';
 import { NzIconTestModule } from 'ng-zorro-antd/icon/testing';
 import { InWeatherAppComponent } from './in-weather-app.component';
+import { By } from '@angular/platform-browser';
 
 describe('InWeatherAppComponent', () => {
   let component: InWeatherAppComponent;
@@ -27,5 +28,23 @@ describe('InWeatherAppComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it(`should render main-layout component`, () => {
+    const mainLayout = fixture.debugElement.query(By.css('app-main-layout'));
+    expect(mainLayout).not.toBeNull();
+  });
+
+  it(`should have Home and Favorite location as menu options`, () => {
+    const menuItemsEl = fixture.debugElement.queryAll(
+      By.css('.menu .ant-menu-item')
+    );
+
+    const renderedMenuItems = menuItemsEl.map(({ nativeElement }) =>
+      (nativeElement as HTMLLIElement).innerText.trim()
+    );
+
+    expect(renderedMenuItems[0]).toBe('en.menu.home');
+    expect(renderedMenuItems[1]).toBe('en.menu.favoriteLocations');
   });
 });
